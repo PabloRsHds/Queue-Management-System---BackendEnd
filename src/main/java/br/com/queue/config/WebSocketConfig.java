bb91c48b082a59dea0d4873afb54681f3dce9078
Pablo <pablorshds@gmail.com>
2026-09-09 20:53:17 -0300

    feat: alterando o backend para adicionar o docker-compose.yaml, e outras configurações para subir tanto o banco como o próprio backend, com isso agora, precisando só alterar o env para conseguir, rodar o projeto

--- src/main/java/br/com/queue/config/WebSocketConfig.java
+++ src/main/java/br/com/queue/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package br.com.queue.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,13 +11,16 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${api.frontend.url}")
+    private String frontendUrl;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
         registry
                 .addEndpoint("/ws")
                 .setAllowedOrigins(
-                        "http://localhost:4200"
+                        frontendUrl
                 )
                 .withSockJS();
     }
